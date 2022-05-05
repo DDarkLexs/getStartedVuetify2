d008de23536695e6c0d7e3b6d4f062413d3ce017
antonio lugogo <antoniolexis2001@gmail.com>
2022-05-05 09:41:54 +0100

    sucesso

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,102 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+<v-app>
+  <v-navigation-drawer
+      app
+      class="primary"
+      dark
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    
+    </v-navigation-drawer>
+
+ <v-app-bar
+      color="primary"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+ 
+      </v-menu>
+    </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+   <v-footer
+    app
+    dark
+    padless>
+
+    <v-card
+      class="flex"
+      flat
+      tile>
+
+      <v-card-text class="py-0 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>DDarkCoder</strong>
+      </v-card-text>
+    </v-card>
+     </v-footer>
+</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+     items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Account', icon: 'mdi-account-box' },
+          { title: 'Admin', icon: 'mdi-gavel' },
+        ],
+    //
+  }),
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@themeConfig': path.resolve(__dirname, 'themeConfig.js'),
+        '@core': path.resolve(__dirname, 'src/@core'),
+        '@validations': path.resolve(__dirname, 'src/@core/utils/validations/validations.js'),
+        '@axios': path.resolve(__dirname, 'src/libs/axios'),
+      },
+    },
+  },
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
